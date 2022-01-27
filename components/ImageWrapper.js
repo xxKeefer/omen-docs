4fd2dcab8161a2f77411a6fefbbcba9c57de0ada
xxKeefer <xxkeefer.code@gmail.com>
2022-01-28 08:25:28 +1000

    added rules for zones

---
+++ components/ImageWrapper.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const ImageWrapper = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding-top: 2rem;
+`
--- theme.config.js
+++ theme.config.js
@@ -22,6 +22,8 @@ export default {
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta name="description" content="Omen: The Story driven RPG" />
             <meta name="title" content="Omen: The Story driven RPG" />
+            <meta name="og:description" content="Omen: The Story driven RPG" />
+            <meta name="og:title" content="Omen: The Story driven RPG" />
         </>
     ),
 }
