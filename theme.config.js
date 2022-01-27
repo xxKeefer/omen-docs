76d7d5e83e5e3e2512020f8dd8c6fd4d4cd77f2d
xxKeefer <xxkeefer.code@gmail.com>
2022-01-28 07:26:28 +1000

    update config

--- theme.config.js
+++ theme.config.js
@@ -1,24 +1,27 @@
 // theme.config.js
 export default {
-  titleSuffix: " - Omen RPG",
-  nextLinks: true,
-  prevLinks: true,
-  search: true,
-  customSearch: null, // customizable, you can use algolia for example
-  darkMode: true,
-  footer: true,
-  footerText: `${new Date().getFullYear()} © Ben Gorman.`,
-  logo: (
-    <>
-      <svg>...</svg>
-      <span>Omen Docs</span>
-    </>
-  ),
-  head: (
-    <>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content="Omen: Story Driven RPG" />
-      <meta name="og:title" content="Omen: Story Driven RPG" />
-    </>
-  ),
-};
+    projectLink: 'https://github.com/xxKeefer/omen-docs',
+    docsRepositoryBase: 'https://github.com/xxKeefer/omen-docs', // base URL for the docs repository
+    titleSuffix: ' - Omen RPG',
+    nextLinks: true,
+    prevLinks: true,
+    search: true,
+    customSearch: null, // customizable, you can use algolia for example
+    darkMode: true,
+    footer: true,
+    footerText: `Omen RPG ${new Date().getFullYear()} © Ben Gorman.`,
+    footerEditLink: false,
+    logo: (
+        <>
+            <svg>...</svg>
+            <span>Omen RPG</span>
+        </>
+    ),
+    head: (
+        <>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="description" content="Omen: The Story driven RPG" />
+            <meta name="title" content="Omen: The Story driven RPG" />
+        </>
+    ),
+}
